5229588066c01ca8d6a0ee8f35a5bab32402f310
jafifd <jafifdbeltran@hotmail.com>
2020-08-03 17:20:45 -0500

    Se sube codigo del dia 3 de Agosto

---
+++ Sesion3_Agosto/src/ico/fes/poo/Sesion3_Agosto.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo;
+
+/**
+ *
+ * @author jafif
+ */
+public class Sesion3_Agosto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Teclado objTeclado1= new Teclado();
+        objTeclado1.teclear(10);
+        Teclado t2= new Teclado();
+        Teclado t3= new Teclado();
+        Teclado t4= new Teclado();
+        Teclado t5= new Teclado();
+    }
+    
+}
---
+++ Sesion3_Agosto/src/ico/fes/poo/Teclado.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo;
+
+/**
+ *
+ * @author jafif
+ */
+class Teclado {
+    int numeroTeclas;
+    
+    public void teclear(int keyCode){
+        System.out.println("Se presiono la tecla:"+keyCode);
+        
+    }
+    
+}
