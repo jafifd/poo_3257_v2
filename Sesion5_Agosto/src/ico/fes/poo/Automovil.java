b1986303e5c684a35f7a82ad486ed64717b89fe0
jafifd <jafifdbeltran@hotmail.com>
2020-08-08 18:51:29 -0500

    se sube primer codigo del automovil

--- Sesion5_Agosto/src/ico/fes/poo/Automovil.java
+++ Sesion5_Agosto/src/ico/fes/poo/Automovil.java
@@ -5,10 +5,74 @@
  */
 package ico.fes.poo;
 
-/**
- *
- * @author jafif
- */
+
 public class Automovil {
+private String Marca;
+private String SubMarca;
+private int modelo;
+private String Color;
+
+//metodos de Acceso
+public void setModelo(int mod ){
+    if(mod >= 1914 && mod <=2020){
+        this.modelo= mod;
+    }else{
+        System.out.println("Ese modelo esta fuera de rango");
+    }
+}
+
+public int setModelo(){
+    return this.modelo;
+}
+public String setMarca(){
+   return Marca;
+}
+public void setMarca(String Marca){
+   this.Marca = Marca;
+}
+public void setSubMarca(String SubMarca){
+    this .SubMarca= SubMarca;
+}
+public String setSubMarca(){
+    return SubMarca;
     
+}
+public void setColor(String Color){
+
+    this.Color = Color;
+   
+    }
+public String setColor(){
+    return Color;
+}
+     
+
+
+
+            
+
+
+
+public void encender (){
+    System.out.println("Encendiendo el auto");
+}
+public void acelerar(){
+   System.out.println("Acelerando");
+
+}
+public float avanzar (int metros ){
+    float reales=0.0f;
+    reales = (float) Math.random()*(float)metros;
+    System.out.println("Avanzando el auto"+this.Marca);
+    return reales;
+    
+    }
+
+    @Override
+    public String toString() {
+        return this.Marca+","+ this.SubMarca +","
+        + this.modelo +","+this.Color ; //To change body of generated methods, choose Tools | Templates.
+    }
+
+
 }
--- Sesion5_Agosto/src/sesion5_agosto/Sesion5_Agosto.java
+++ Sesion5_Agosto/src/sesion5_agosto/Sesion5_Agosto.java
@@ -5,6 +5,8 @@
  */
 package sesion5_agosto;
 
+import ico.fes.poo.Automovil;
+
 /**
  *
  * @author jafif
@@ -15,7 +17,36 @@ public class Sesion5_Agosto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     Automovil miCarro=new Automovil();
+     //miCarro.Marca=" chevrolet";
+     //miCarro.SubMarca="cavalier";
+     //miCarro.modelo=2016;
+     //miCarro.color="negro";
+     miCarro.setModelo(2075);
+     float x = miCarro.avanzar(100);
+        System.out.println("Avanzados="+x);
+        
+        Automovil miCarroDos= new Automovil();
+        //miCarroDos.Marca="ford";
+        //miCarroDos.SubMarca="Mustang";
+        //miCarroDos.color="Amarillo";
+        //miCarroDos.modelo= 2010;
+        miCarroDos.setModelo(2010);
+        System.out.println("Avanzados="+ miCarroDos.avanzar(500));
+        
+        System.out.println(miCarro);
+        System.out.println(miCarroDos);
+        
+        
+        Automovil miCarroTres= new Automovil();
+        //miCarroTres.Marca ="Acura";
+        //miCarroTres.SubMarca="NSX";
+        //miCarroTres.color="gris";
+        //miCarroTres.modelo=2013;
+        System.out.println("Avanzados="+ miCarroTres.avanzar(500));
+        
+        System.out.println(miCarro.toString());          
+// TODO code application logic here
     }
     
 }
