ce75b38341904d08cebdb8846e92507efbe85b54
jafifd <jafifdbeltran@hotmail.com>
2020-08-10 16:54:23 -0500

    Se sube codigo de la Clase 10 de Agosto

---
+++ Sesion_10deAgosto/src/sesion_10_agosto/Sesion_10deAgosto.java
@@ -0,0 +1,19 @@
+
+package sesion_10_agosto;
+
+import ico.fee.poo.Smartphone;
+
+
+public class Sesion_10deAgosto {
+
+    
+    public static void main(String[] args) {
+        Smartphone tell = new Smartphone();
+        System.out.println(tell);
+        Smartphone tel2 = new Smartphone("Samsung", "Galaxy","Smart Dragon", 2, 
+                "Android 7.0", 23.4f);
+        
+        System.out.println(tel2);
+    }
+    
+}
